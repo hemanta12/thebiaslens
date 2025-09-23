6ea7d6df9fac1ad984d2ba2d5c5bd216c2d09fdd
hemanta12 <hemantathapa@Hemantas-MacBook-Pro.local>
2025-09-22 17:04:26 -0700

    add support for vercel

--- api/main.py
+++ api/main.py
@@ -15,6 +15,7 @@ app = FastAPI()
 allowed_origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+     "https://thebiaslens.vercel.app",
 ]
 
 # Add regex pattern for Vercel preview URLs
